c524014d3738e3b945e69aec2a374605d866efee
David Soler <davisorb95@gmail.com>
2021-06-25 15:12:47 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeng-app';
+export class AppComponent implements OnInit {
+  itemsMenu: MenuItem[] = [];
+  itemsPanel: MenuItem[] = [];
+  value: Date = new Date();
+  display: boolean = false;
+
+  constructor(
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.itemsMenu = [
+      {
+        label: 'File',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'Project' },
+            { label: 'Other' },
+          ]
+        },
+        { label: 'Open' },
+        { label: 'Quit' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      }
+    ];
+
+    this.itemsPanel = [
+      {
+        label: 'File',
+        icon: 'pi pi-pw pi-file',
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'User', icon: 'pi pi-fw pi-user-plus' },
+            { label: 'Filter', icon: 'pi pi-fw pi-filter' }
+          ]
+        },
+        { label: 'Open', icon: 'pi pi-fw pi-external-link' },
+        { separator: true },
+        { label: 'Quit', icon: 'pi pi-fw pi-times' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      },
+      {
+        label: 'Help',
+        icon: 'pi pi-fw pi-question',
+        items: [
+          {
+            label: 'Contents',
+            icon: 'pi pi-pi pi-bars'
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-pi pi-search',
+            items: [
+              {
+                label: 'Text',
+                items: [
+                  {
+                    label: 'Workspace'
+                  }
+                ]
+              },
+              {
+                label: 'User',
+                icon: 'pi pi-fw pi-file',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        label: 'Actions',
+        icon: 'pi pi-fw pi-cog',
+        items: [
+          {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+              { label: 'Save', icon: 'pi pi-fw pi-save' },
+              { label: 'Update', icon: 'pi pi-fw pi-save' },
+            ]
+          },
+          {
+            label: 'Other',
+            icon: 'pi pi-fw pi-tags',
+            items: [
+              { label: 'Delete', icon: 'pi pi-fw pi-minus' }
+            ]
+          }
+        ]
+      }
+    ];
+  }
+
+  addSingle() {
+    this.messageService.add({ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' });
+  }
+
+  addMultiple() {
+    this.messageService.addAll([{ severity: 'success', summary: 'Service Message', detail: 'Via MessageService' },
+    { severity: 'info', summary: 'Info Message', detail: 'Via MessageService' }]);
+  }
+
+  clear() {
+    this.messageService.clear();
+  }
+
+  showDialog() {
+    this.display = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+// PrimeNg
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 import { AppComponent } from './app.component';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    PrimeNgModule
   ],
-  providers: [],
+  providers: [
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
