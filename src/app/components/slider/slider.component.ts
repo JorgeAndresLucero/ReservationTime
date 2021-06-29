2334960676338b68f24eff4ac8d4b99e713977c5
Jorge Andrés Lucero Hernández <jlucero@asesoftware.com>
2021-06-29 16:27:14 -0500

    Adding the responsive action for hour label while is sliding

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -7,12 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent {
   title = 'Slider';
-  val: number=0;
+  val: number=7;
+  minutes: string="";
+  day_time: string ="am";
   rangeValues: number[] = [7,18];
 
+  hour(val: number){
+    val >= 12 ?  this.day_time = "pm" : this.day_time = "am";
+    return this.day_time;
+  }
+  
+  transformation(val: number){
+    Number.isInteger(val) ?  (this.minutes = this.val.toString()+":00") : this.minutes = Number.parseInt(this.val.toString())+":30";
+    return this.minutes;
+  }
+  
+
+ 
   constructor() {
-
-
    }
+
+   
 
 }
